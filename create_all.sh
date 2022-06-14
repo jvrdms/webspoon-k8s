fe128c059cfee04adb493ce00d93a4fbf7bc0b47
jvrdms <jvrdms@gmail.com>
2022-06-14 19:13:50 +0200

    namespace added

--- create_all.sh
+++ create_all.sh
@@ -3,7 +3,9 @@
 kubectl create configmap webspoon-config-cm \
   --from-file=config/web.xml \
   --from-file=config/catalina.policy
+
 [ ! -s config/security.xml ] && cp config/security.xml{.back,}
+
 kubectl create secret generic webspoon-secret \
   --from-file=config/security.xml
 kubectl apply -f .
